9d2d27dddff2f6ebfc6b86915d2ee699971648fd
Brian Vuu <brianvuu@gmail.com>
2018-11-20 00:39:41 -0700

    section updates

--- src/App.js
+++ src/App.js
@@ -5,20 +5,31 @@ import styled from 'styled-components';
 class App extends Component {
   render() {
     return (
+
       <div className="container">
+
         <div className="intro">
           <h1>Intro</h1>
         </div>
+
         <div className="skills">
           <h1>Skills</h1>
         </div>
+
         <div className="resume">
           <h1>Resume</h1>
         </div>
-        <div className="footer">
-          <h1>Footer</h1>
+
+        <div className="contact">
+          <h1>Contact</h1>
         </div>
+
+        <div className="footer">
+          <p>brian-vuu.com | Created by Brian Vuu | 2018</p>
+        </div>
+
       </div>
+
     );
   }
 }
